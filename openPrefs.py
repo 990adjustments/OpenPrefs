9ba5ff069ad583964f0b0659300b0cab4154c5f0
Erwin Santacruz <escruz@gmail.com>
2012-02-23 04:51:15 -0500

    Initial commit.

---
+++ openPrefs.py
@@ -0,0 +1,26 @@
+"""
+OpenPrefs
+
+Copyright: Erwin Santacruz, www.990adjustments.com
+Written for CINEMA 4D R12.016
+
+Name-US: OpenPrefs
+Description-US: Open the Cinema 4D user preferences folder.
+Make it a button for quick access.
+
+Script tested on OS X 10.6.4 and Cinema 4D versions:
+CINEMA 4D R12.016
+CINEMA 4D R12.032
+
+Creation Date: 02/21/2012
+"""
+
+import c4d
+from c4d import storage
+
+
+def main():
+    storage.ShowInFinder(storage.GeGetC4DPath(c4d.C4D_PATH_PREFS))
+
+if __name__=='__main__':
+    main()
